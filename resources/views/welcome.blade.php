6ff30d7f6cd076fe7e07050b4ec8e7b672aee5f3
Harsh Davra <harshdavra1@gmail.com>
2025-12-07 16:18:42 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AI-Trip-Planner</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/style.css') }}">
+</head>
+<body>
+    <div>
+        <nav>
+            <div>
+                <img src="{{asset('images/road-map.png') }}" alt="" srcset="">
+                <h4>Plan-Your-Trip</h4>
+            </div>
+            <h4>Trending Trips</h4>
+
+        </nav>
+    </div>
+    
+</body>
+</html>
