178d64897e8d6cb6cb40a35de45226b79c6ee2b7
Harsh Davra <harshdavra1@gmail.com>
2025-12-17 22:14:32 +0530

    build background and index more design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,17 +10,17 @@
 </head>
 
 <body>
-    <div class="align-items-center justify-content-center">
+    <div class="box align-items-center justify-content-center d-flex flex-column">
 
-        <nav class="d-flex sticky-top align-items-center justify-content-between p-2 navbar">
-            <div class="d-flex align-items-center gap-2 ms-5">
+        <nav class="d-flex sticky-top align-items-center justify-content-between navbar w-100">
+            <div class="nav-logo d-flex align-items-center gap-2 ms-5">
                 <img src="{{asset('images/road-map.png') }}" alt="" srcset="" class="img-fluid">
                 <h4 class="m-0">Plan-Your-Trip</h4>
             </div>
-            <h5 class="m-0 me-5">Trending Trips</h4>
+            <h5 class="trending-title m-0 me-5">Trending Trips</h5>
         </nav>
 
-        <div class="content d-flex justify-content-center flex-column align-items-center gap-4 mt-4">
+        <div class="content d-flex justify-content-center flex-column align-items-center gap-4 mt-5">
             <div class="planner d-flex align-items-center gap-3">
                 <img src="{{asset('images/Ai-harsh.jpg')}}" alt="">
                 <h2 class="Ai-name m-0">Harsh</h2>
@@ -37,6 +37,7 @@
         </div>
 
         <div class="trending-trip">
+            <h1>Trending-Trips</h1>
             <div class="trip-card">
                 <img src="{{asset('images/miami.jpg')}}" alt="Chile Trip">
                 <div class="trip-body">
@@ -107,6 +108,11 @@
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div class="trip-topic d-flex flex-column align-items-center">
+            <h1>Why You Go On a Trip?</h1>
+            <p class="p-3">Travel allows you to take a break from everyday life and refresh your mind and body. It helps you explore new cultures, gain new perspectives, and build confidence through new experiences. Every journey creates lasting memories that enrich your life and inspire personal growth.</p>
         </div>
 
     </div>
