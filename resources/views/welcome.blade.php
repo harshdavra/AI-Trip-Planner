57b7b19a504e8e7b2acdaccb9205d845a8b556f4
Harsh Davra <harshdavra1@gmail.com>
2025-12-30 22:28:17 +0530

    build responsive design for mobile and tablet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,11 @@
     <title>AI-Trip-Planner</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css') }}">
+    
 </head>
 
 <body>
-    <div class="box align-items-center justify-content-center d-flex flex-column">
-
+    <div class="container-fluid d-flex align-items-center flex-column m-0 p-0">
         <nav class="d-flex sticky-top align-items-center justify-content-between navbar w-100">
             <div class="nav-logo d-flex align-items-center gap-2 ms-5">
                 <img src="{{asset('images/road-map.png') }}" alt="" srcset="" class="img-fluid">
@@ -110,9 +110,39 @@
             </div>
         </div>
 
-        <div class="trip-topic d-flex flex-column align-items-center">
-            <h1>Why You Go On a Trip?</h1>
-            <p class="p-3">Travel allows you to take a break from everyday life and refresh your mind and body. It helps you explore new cultures, gain new perspectives, and build confidence through new experiences. Every journey creates lasting memories that enrich your life and inspire personal growth.</p>
+        <div class="trip-topic">
+            <div class="trip-topic-why d-flex flex-column align-items-center">
+                <h1>📷 Why You Go On a Trip? 📷</h1>
+                <p class="p-3">Travel allows you to take a break from everyday life and refresh your mind and body. It helps you explore new cultures, gain new perspectives, and build confidence through new experiences. Every journey creates lasting memories that enrich your life and inspire personal growth.</p>
+            </div>
+            <div class="topics">
+  <div class="topic-item right">
+    <h4>1️⃣ Smart Trip Planning</h4>
+    <p>Compare top flight options and find the best hotels all in one place. Choose stays that match your comfort level and budget with clear pricing and no hidden costs. Enjoy smart suggestions for every destination and book your travel with complete confidence and flexibility.</p>
+  </div>
+
+  <div class="topic-item left">
+    <h4>2️⃣ Best Flights & Hotels</h4>
+    <p>Compare top flight options and find the best hotels all in one place. Choose stays that match your comfort level and budget with clear pricing and no hidden costs. Enjoy smart suggestions for every destination and book your travel with complete confidence and flexibility.</p>
+  </div>
+
+  <div class="topic-item right">
+    <h4>3️⃣ Handpicked Experiences</h4>
+    <p>Discover local attractions and hidden gems that make each destination special. Enjoy food, culture, and adventure curated to your travel style. These experiences are designed to make every trip memorable and help you explore beyond the usual tourist spots.</p>
+  </div>
+
+  <div class="topic-item left">
+    <h4>4️⃣ Trips for Every Traveler</h4>
+    <p>Whether you’re traveling with family, going solo, or planning a romantic getaway, there’s a trip designed for you. Find safe and comfortable plans for solo travelers, adventure-packed trips for thrill seekers, and peaceful vacations for those who want to relax and unwind..</p>
+  </div>
+
+  <div class="topic-item right">
+    <h4>5️⃣ Travel Made Simple</h4>
+    <p>Everything you need for your journey is available on one easy-to-use platform. Plan, customize, and manage your trips effortlessly with clear day-by-day itineraries. Enjoy stress-free travel from start to finish and focus on creating memories instead of planning.</p>
+  </div>
+</div>
+
+          
         </div>
 
     </div>
